c70bb847f9d2e000583a49ba333704ce340a17ee
Manny <manny@His-Mac.local>
2020-06-20 09:15:54 -0400

    FIX: reverting code back

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 80;
 const VERSION = process.env.VERSION || '1.0.0';
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const APP_VERSION_LABEL = process.env.APP_VERSION_LABEL || '';
-const DEPLOYMENT_ID = process.env.DEPLOYMENT_ID || '';
-const SERIAL = process.env.SERIAL || '';
 
 // Configurations
 // ========================================
@@ -22,10 +19,7 @@ app.use(cors());
 app.get('/', (_req, res) =>
   res.send({
     version: VERSION,
-    environment: ENVIRONMENT,
-    appVersionLabel: APP_VERSION_LABEL,
-    deploymentId: DEPLOYMENT_ID,
-    serial: SERIAL
+    environment: ENVIRONMENT
   })
 );
 
